488b41ea0a1d4da492693c37fed90b6ad0167198
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-09-11 15:42:20 +0200

    Update theme_manager.dart

--- lib/core/theme/theme_manager.dart
+++ lib/core/theme/theme_manager.dart
@@ -4,13 +4,13 @@ import 'package:m_hany_store/core/theme/colors/color_theme.dart';
 
 ThemeData getAppThemeData(){
   return ThemeData(
-    primaryColor: ColorTheme.darkBackroundPage,
-    brightness: Brightness.dark,
     // scaffoldBackgroundColor:  ColorTheme.backgroundColorPage,
     appBarTheme: const AppBarTheme(
       backgroundColor: ColorTheme.darkBackroundPage,
       systemOverlayStyle: SystemUiOverlayStyle(
-        statusBarColor:  ColorTheme.darkBackroundPage,
+      statusBarIconBrightness: Brightness.light,
+        statusBarColor:  Colors.red,
+        statusBarBrightness: Brightness.light
       ),
     ),
   );
