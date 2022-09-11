5761402a2015a5f0dab79bc0bdf418ffa0555ff8
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-09-11 15:42:27 +0200

    Update product_item_widget.dart

--- lib/user/product/product_widget/product_item_widget.dart
+++ lib/user/product/product_widget/product_item_widget.dart
@@ -1,52 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:m_hany_store/core/theme/colors/color_theme.dart';
-import 'package:m_hany_store/core/theme/fonts/style.dart';
+import 'package:m_hany_store/core/routes/string_route.dart';
 
 
 class ProductItemWidget extends StatelessWidget {
   const ProductItemWidget({super.key});
 // NetworkImage('https://media.gettyimages.com/photos/commuters-cross-a-main-road-next-to-an-advertisement-of-console-game-picture-id180851513?s=612x612') 
+
+Widget product(){
+  return Container(
+    width: 170,
+    height: 222,
+    decoration: BoxDecoration(
+      borderRadius:BorderRadius.circular(8),
+      //  color: Colors.black,
+      image: const DecorationImage(
+        fit: BoxFit.fill,
+        image: NetworkImage(
+          'https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/1280/V.jpg'
+        ),
+      ),
+    ),
+  );
+}
+  Widget listProduct(){
+    return Row(
+      children: [
+        product(),
+        const SizedBox(
+          width: 15,
+        ),
+        product(),
+      ],
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 160,
-      height: 220,
-      decoration: BoxDecoration(
-        color: ColorTheme.primary,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-              width: 160,
-              height: 180,
-            decoration: BoxDecoration(
-              borderRadius:BorderRadius.circular(8),
-              //  color: Colors.black,
-              image: const DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                  'https://media.gettyimages.com/photos/commuters-cross-a-main-road-next-to-an-advertisement-of-console-game-picture-id180851513?s=612x612'
-                ),
-              ),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(12, 12, 12, 12),
+        child: Column(
+          children: [
+            InkWell(
+              onTap: ()=> Navigator.pushNamed(context, previewProductPage),
+              child: listProduct()
             ),
-          ),
-          Padding(
-             padding: const EdgeInsets.fromLTRB(8, 3, 0, 0),
-             child: Text(
-              'Gta v',
-               style: getMediumStyle(color: ColorTheme.white),
+            const SizedBox(
+              height: 12,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8, 3, 0, 0),
-            child: Text(
-              '140 LE',
-              style: getMediumStyle(color: ColorTheme.white),
+            listProduct(),
+            const SizedBox(
+              height: 12,
             ),
-          ),
-        ],
+            listProduct(),
+            const SizedBox(
+              height: 12,
+            ),
+           listProduct(),
+            const SizedBox(
+              height: 12,
+            ),
+          ],
+        ),
       ),
     );
   }
