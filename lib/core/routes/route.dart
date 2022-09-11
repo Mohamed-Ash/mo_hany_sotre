3569f7e0445b4843f578f27b01eda66e90d4741c
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-09-11 15:42:00 +0200

    Update route.dart

--- lib/core/routes/route.dart
+++ lib/core/routes/route.dart
@@ -3,14 +3,19 @@ import 'package:m_hany_store/core/auth/login/login_page.dart';
 import 'package:m_hany_store/core/auth/register/register_page/register_page.dart';
 import 'package:m_hany_store/core/page/app_page_layout.dart';
 import 'package:m_hany_store/core/routes/string_route.dart';
+import 'package:m_hany_store/user/product/product_page/preview_product_page.dart';
 
 class Routes{
 
   Route? generateRoute(RouteSettings settings){
     switch (settings.name) {
-      case appPageLayout:                                       return MaterialPageRoute(builder: (_)=> const AppPageLayout());
+      // todo: auth
       case registerPage:                                        return MaterialPageRoute(builder: (_)=>  RegisterPage());
       case loginPage:                                           return MaterialPageRoute(builder: (_)=>  LoginPage());
+      
+      // todo: user
+      case appPageLayout:                                       return MaterialPageRoute(builder: (_)=> const AppPageLayout());
+      case previewProductPage:                                       return MaterialPageRoute(builder: (_)=> const PreviewProductPage());
         
       default:
     }
