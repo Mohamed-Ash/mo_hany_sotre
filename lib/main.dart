e7de797c36a1491711805b730178c78e1ea3fbf5
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-09-15 14:53:15 +0200

    add fire base on main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:m_hany_store/core/app.dart';
 import 'package:m_hany_store/core/routes/route.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:m_hany_store/firebase_options.dart';
+
+
 //6610f2
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp( App(routes: Routes(),));
   
 }
