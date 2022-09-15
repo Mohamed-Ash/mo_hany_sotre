cd48a2cf08bc1ac4ab02d615b02ff133a967dc9b
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-09-15 14:56:24 +0200

    create checkout page

---
+++ lib/core/payments/checkout_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:m_hany_store/core/payments/checkout_widget.dart';
+import 'package:m_hany_store/core/theme/fonts/style.dart';
+import 'package:m_hany_store/user/user_interface.dart';
+
+class CheckoutPage extends UserInterface{
+  const CheckoutPage({super.key});
+
+  @override
+  PreferredSizeWidget? get appBar => AppBar(
+    elevation: 0,
+    leading: const Text(''),
+    title: Text(
+      'Checkout',
+      style: getBoldStyle(color: Colors.white,fontSize: 22),
+    ),
+  );
+  
+  @override
+  Widget buildBody(BuildContext context) {
+    return const CheckoutWidget();
+  }
+}
