4a910ae9f611687119cded1a5ebfb5c041b0724e
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-09-24 01:22:48 +0200

    Update preview_product_page.dart

--- lib/user/product/product_page/preview_product_page.dart
+++ lib/user/product/product_page/preview_product_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:m_hany_store/user/product/product_page/preview_product_widget.dart';
+import 'package:m_hany_store/user/product/product_widget/preview_product_widget.dart';
 
 import 'package:m_hany_store/user/user_interface.dart';
 
