cf576ddd0f5ce837f152d230a4ac158679df72fa
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-09-24 01:22:38 +0200

    add new pages on route

--- lib/core/routes/route.dart
+++ lib/core/routes/route.dart
@@ -1,25 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:m_hany_store/admin/discounts_product/discounts_product_page/discounts_product_page.dart';
+import 'package:m_hany_store/admin/home/admin_home.dart';
+import 'package:m_hany_store/admin/product/admin_product_page/admin_product_page.dart';
 import 'package:m_hany_store/core/auth/login/login_page.dart';
 import 'package:m_hany_store/core/auth/register/register_page/register_page.dart';
 import 'package:m_hany_store/core/page/app_page_layout.dart';
 import 'package:m_hany_store/core/payments/checkout_page.dart';
 import 'package:m_hany_store/core/routes/string_route.dart';
 import 'package:m_hany_store/user/product/product_page/preview_product_page.dart';
+import 'package:m_hany_store/user/sale/sale_page/preview_sale_page.dart';
 
 class Routes{
 
   Route? generateRoute(RouteSettings settings){
     switch (settings.name) {
       // todo: auth
-      case registerPage:                                        return MaterialPageRoute(builder: (_)=>  RegisterPage());
+      case registerPage:                                        return MaterialPageRoute(builder: (_)=>  const RegisterPage());
       case loginPage:                                           return MaterialPageRoute(builder: (_)=>  LoginPage());
       
       // todo: user
       case appPageLayout:                                       return MaterialPageRoute(builder: (_)=> const AppPageLayout());
-      case previewProductPage:                                       return MaterialPageRoute(builder: (_)=> const PreviewProductPage());
+      case previewProductPage:                                  return MaterialPageRoute(builder: (_)=> const PreviewProductPage());
+      case previewSalePage:                                     return MaterialPageRoute(builder: (_)=>  PreviewSalePage());
         
       // todo: payment methods
-      case checkoutpage:                                       return MaterialPageRoute(builder: (_)=> const CheckoutPage());
+      case checkoutpage:                                        return MaterialPageRoute(builder: (_)=> const CheckoutPage());
+
+      // todo: admin
+
+      case adminHome:                                           return MaterialPageRoute(builder: (_)=> const AdminHome());
+      case adminProductPage:                                    return MaterialPageRoute(builder: (_)=> const AdminProductPage());
+      case discountsProduct:                                    return MaterialPageRoute(builder: (_)=> const DiscountsProductPage());
+
+
+
+
 
       default:
     }
--- lib/core/routes/string_route.dart
+++ lib/core/routes/string_route.dart
@@ -5,9 +5,20 @@ const String loginPage = '/'; //login_page
   // todo : user
 const String appPageLayout = '/app_page_layout';
 const String previewProductPage = '/preview_product_page';
+const String previewSalePage = '/preview_sale_page';
 
 
 
   // todo : payment methods
 
 const String checkoutpage = '/checkout_page';
+  
+  
+  
+  
+   // todo : user
+   
+const String adminHome            = '/admin_home';
+const String adminProductPage     = '/admin_product_page';
+const String discountsProduct     = '/discounts_product';
+   
