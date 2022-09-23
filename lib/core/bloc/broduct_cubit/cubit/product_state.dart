893b86325d67a367cf9b85809eee5403f6c34728
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-09-24 01:31:55 +0200

    create product bloc

---
+++ lib/core/bloc/broduct_cubit/cubit/product_state.dart
@@ -0,0 +1,20 @@
+/* part of 'product_cubit.dart';
+
+@immutable
+abstract class ProductState {}
+
+class ProductInitial extends ProductState {
+
+}
+
+// ignore: must_be_immutable
+class ProductLoadedState extends ProductState {
+  List<dynamic> products = [];
+
+  ProductLoadedState(products);
+}
+
+class ProductLoadingState extends ProductState {
+
+}
+ */
