43ab278a27c6d7519153fb978b6e563755d7e45f
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-09-27 12:25:52 +0200

    add  new pages

--- lib/core/routes/route.dart
+++ lib/core/routes/route.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:m_hany_store/admin/discounts_product/discounts_product_page/discounts_product_page.dart';
+import 'package:m_hany_store/admin/categories/categories_page/add_item_categories_page.dart';
+import 'package:m_hany_store/admin/categories/categories_page/categories_page.dart';
+import 'package:m_hany_store/admin/categories/categories_page/edit_item_categories_page.dart';
+import 'package:m_hany_store/admin/categories/categories_page/preview_item_categories_page.dart';
 import 'package:m_hany_store/admin/home/admin_home.dart';
-import 'package:m_hany_store/admin/product/admin_product_page/admin_product_page.dart';
-import 'package:m_hany_store/core/auth/login/login_page.dart';
+import 'package:m_hany_store/admin/offers/offers_page/edit_item_offers_page.dart';
+import 'package:m_hany_store/admin/offers/offers_page/offers_page.dart';
+import 'package:m_hany_store/admin/offers/offers_page/preview_item_offers_page.dart';
+import 'package:m_hany_store/admin/product/product_page/add_item_product_page.dart';
+import 'package:m_hany_store/admin/product/product_page/edit_item_product_page.dart';
+import 'package:m_hany_store/admin/product/product_page/admin_product_page.dart';
+import 'package:m_hany_store/admin/product/product_page/preview_item_product_page.dart';
+import 'package:m_hany_store/admin/shipping/shipping_page/edit_item_shipping_page.dart';
+import 'package:m_hany_store/admin/shipping/shipping_page/preview_item_shipping_page.dart';
+import 'package:m_hany_store/admin/shipping/shipping_page/shipping_page.dart';
+import 'package:m_hany_store/core/auth/login/login_page/login_page.dart';
 import 'package:m_hany_store/core/auth/register/register_page/register_page.dart';
 import 'package:m_hany_store/core/page/app_page_layout.dart';
 import 'package:m_hany_store/core/payments/checkout_page.dart';
 import 'package:m_hany_store/core/routes/string_route.dart';
-import 'package:m_hany_store/user/product/product_page/preview_product_page.dart';
+import 'package:m_hany_store/user/categories/product/product_page/preview_product_page.dart';
+import 'package:m_hany_store/user/categories/product/product_page/product_page.dart';
 import 'package:m_hany_store/user/sale/sale_page/preview_sale_page.dart';
 
 class Routes{
@@ -21,7 +34,8 @@ class Routes{
       // todo: user
       case appPageLayout:                                       return MaterialPageRoute(builder: (_)=> const AppPageLayout());
       case previewProductPage:                                  return MaterialPageRoute(builder: (_)=> const PreviewProductPage());
-      case previewSalePage:                                     return MaterialPageRoute(builder: (_)=>  PreviewSalePage());
+      case showSalePage:                                        return MaterialPageRoute(builder: (_)=>  PreviewSalePage());
+      case productPagse:                                        return MaterialPageRoute(builder: (_)=>  const ProductPage());
         
       // todo: payment methods
       case checkoutpage:                                        return MaterialPageRoute(builder: (_)=> const CheckoutPage());
@@ -29,8 +43,19 @@ class Routes{
       // todo: admin
 
       case adminHome:                                           return MaterialPageRoute(builder: (_)=> const AdminHome());
-      case adminProductPage:                                    return MaterialPageRoute(builder: (_)=> const AdminProductPage());
-      case discountsProduct:                                    return MaterialPageRoute(builder: (_)=> const DiscountsProductPage());
+      case adminProductsPage:                                   return MaterialPageRoute(builder: (_)=> const AdminProductsPage());
+      case previewItemProductPage:                              return MaterialPageRoute(builder: (_)=> const PreviewItemProductPage());
+      case editItemProductPage:                                 return MaterialPageRoute(builder: (_)=> const EditItemProductPage());
+      case categoriePage:                                       return MaterialPageRoute(builder: (_)=> const CategoriePage());
+      case shippingPage:                                        return MaterialPageRoute(builder: (_)=> const ShippingPage());
+      case offersPage:                                          return MaterialPageRoute(builder: (_)=> const OffersPage());
+      case addItemProductPage:                                  return MaterialPageRoute(builder: (_)=> const AddItemProductPage());
+      case addItemCategoriesPage:                               return MaterialPageRoute(builder: (_)=> const AddItemCategoriesPage());
+      case editItemCategoriesPage:                              return MaterialPageRoute(builder: (_)=> const EditItemCategoriesPage());
+      case previewItemCategoriesPage:                           return MaterialPageRoute(builder: (_)=> const PreviewItemCategoriesPage());
+      case previewItemShippingPage:                             return MaterialPageRoute(builder: (_)=> const PreviewItemShippingPage());
+      case editItemShippingPage:                                return MaterialPageRoute(builder: (_)=> const EditItemShippingPage());
+      case editItemOffersPage:                                  return MaterialPageRoute(builder: (_)=> const EditItemOffersPage());
 
 
 
--- lib/core/routes/string_route.dart
+++ lib/core/routes/string_route.dart
@@ -5,7 +5,8 @@ const String loginPage = '/'; //login_page
   // todo : user
 const String appPageLayout = '/app_page_layout';
 const String previewProductPage = '/preview_product_page';
-const String previewSalePage = '/preview_sale_page';
+const String showSalePage = '/show_sale_page';
+const String productPagse = '/preview_sale_page';
 
 
 
@@ -16,9 +17,21 @@ const String checkoutpage = '/checkout_page';
   
   
   
-   // todo : user
+   // todo : admin 
    
-const String adminHome            = '/admin_home';
-const String adminProductPage     = '/admin_product_page';
-const String discountsProduct     = '/discounts_product';
+const String adminHome                               = '/admin_home';
+const String adminProductsPage                       = '/preview_admin_products_page';
+const String previewItemProductPage                  = '/preview_item_product_page';
+const String editItemProductPage                     = '/edit_item_product_page';
+const String categoriePage                           = '/categorie_page';
+const String shippingPage                            = '/shipping_page';
+const String offersPage                              = '/offers_page';
+const String addItemProductPage                      = '/add_item_product_page';
+const String addItemCategoriesPage                   = '/add_item_categories_page';
+const String editItemCategoriesPage                  = '/edit_item_categories_page';
+const String previewItemCategoriesPage               = '/preview_item_categories_page';
+const String previewItemShippingPage                 = '/preview_item_shipping_page';
+const String editItemShippingPage                    = '/edit_item_shipping_page';
+const String previewItemOffersPage                   = '/preview_item_offers_page';
+const String editItemOffersPage                   = '/preview_item_offers_page';
    
