6471c8043af0affb2b53819052d337cacb88824d
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-09-27 12:30:47 +0200

    Create edit_item_shipping_page.dart

---
+++ lib/admin/shipping/shipping_page/edit_item_shipping_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:m_hany_store/admin/admin_interface.dart';
+
+class EditItemShippingPage extends AdminInterface{
+  const EditItemShippingPage({super.key});
+
+  @override
+  Widget buildBody(BuildContext context) {
+    return Column();
+  }
+} 
