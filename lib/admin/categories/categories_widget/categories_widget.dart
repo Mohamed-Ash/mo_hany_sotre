86f6348b835155a9c08c186e1af345887c106be9
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-09-28 20:08:07 +0200

    get list categories data

--- lib/admin/categories/categories_widget/categories_widget.dart
+++ lib/admin/categories/categories_widget/categories_widget.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:m_hany_store/admin/categories/categories_page/edit_item_categories_page.dart';
+import 'package:m_hany_store/admin/categories/categories_page/preview_item_categories_page.dart';
 import 'package:m_hany_store/core/form_fields/button_form_feilds.dart';
 import 'package:m_hany_store/core/routes/string_route.dart';
 import 'package:m_hany_store/core/theme/colors/color_theme.dart';
@@ -28,34 +33,42 @@ class _CategoriesWidgetState extends State<CategoriesWidget> {
     return Padding(
       padding: const EdgeInsets.fromLTRB(12, 33, 12, 33),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           const SizedBox(height: 22,),
           StreamBuilder(
             stream: getAllProductSale.snapshots(),
-            builder: (context, snapshot) {
+            builder: (context,AsyncSnapshot<QuerySnapshot>  snapshot) {
+              // List<DocumentSnapshot> docs = snapshot.data!.docs;
               if(snapshot.hasData) {
-                return ListView.separated(
-                  scrollDirection: Axis.vertical,
-                  // physics:  NeverScrollableScrollPhysics(),
-                  primary: false,
-                  shrinkWrap: true,
-                  // scrollDirection: Axis.horizontal,
-                  itemBuilder: (context ,index)=> buildItemProduct(context, index), 
-                  separatorBuilder: (context ,index)=> const Divider(
-                    color: ColorTheme.porder,
-                    thickness: 1,
+                return Expanded(
+                  child: ListView.separated(
+                    physics: const ScrollPhysics(),
+                    shrinkWrap: true,
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (context ,index) => buildItemProduct(
+                      getAllProductSale: getAllProductSale,
+                      categories: snapshot.data!.docs[index],
+                      context: context, 
+                      // index: index, 
+                      id: snapshot.data!.docs[index].id
+                    ),
+                    separatorBuilder: (context ,index)=> const Divider(
+                      color: ColorTheme.porder,
+                      thickness: 1,
+                    ),
+                    itemCount: snapshot.data!.docs.length,
                   ),
-                  itemCount: salePRoducts.length,
                 );
-              }else if(snapshot.connectionState == ConnectionState.waiting){
-               return const Center(child:  CircularProgressIndicator(),);
-              }else {
+              } else if(snapshot.connectionState == ConnectionState.waiting){
+                return const Center(child:  CircularProgressIndicator(),);
+              } else {
                 return const Text('error 404');
               }
             },
           ),
-          const Spacer(),
+          // const Spacer(),
+          const SizedBox(height: 22,),
           InkWell(
             onTap: () => Navigator.pushNamed(context, addItemCategoriesPage),
             child: FormFeilds.buttonFormField(
@@ -69,36 +82,80 @@ class _CategoriesWidgetState extends State<CategoriesWidget> {
       ),
     );
   }
-  Widget buildItemProduct(context,index){
+  Widget buildItemProduct({
+    required BuildContext context,
+    // required int index,
+    required String id,
+    required DocumentSnapshot categories,
+    getAllProductSale
+  }){
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Container(
-          width: 100,
-          height: 100,
+          width: 75,
+          height: 75,
           decoration: BoxDecoration(
             // color: Colors.white,
             borderRadius: BorderRadius.circular(12),
             image:  DecorationImage(
               fit: BoxFit.contain,
-              image: NetworkImage('${salePRoducts[index]['images']}')
+              image: NetworkImage('${categories['images']}')
             ),
           ),
         ),
-        const SizedBox(width: 33,),
-        Text('${salePRoducts[index]['title']}',style: getSemiBoldStyle(color: ColorTheme.white,fontSize: 18),),
+        
+        const SizedBox(width: 18,),
+        Flexible(
+          // flex: 3,
+          child: Text(
+            '${categories['name']}',
+            style: getSemiBoldStyle(color: ColorTheme.white,fontSize: 14,),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
         const Spacer(),
+        // const SizedBox(width: 12,),
         Row(
           children: [ 
             IconButton(
-              onPressed: () =>Navigator.pushNamed(context, previewItemCategoriesPage) , 
+              onPressed: () =>Navigator.push(
+                context, MaterialPageRoute(
+                  builder: (context){
+                    return PreviewItemCategoriesPage(id: id,categories: categories,);
+                  }
+                ),
+              ),
               icon: FormFeilds.containerImage(assetImage: 'assets/images/eye.png',height: 18,width: 18),
             ),
             IconButton(
-              onPressed: () => Navigator.pushNamed(context, editItemCategoriesPage),
+              // onPressed: () => Navigator.pushNamed(context, editItemCategoriesPage,arguments: id),
+              onPressed: () => Navigator.push(
+                context, MaterialPageRoute(
+                  builder: (context){
+                    return EditItemCategoriesPage(id: id,categories: categories,);
+                  }
+                ),
+              ),
               icon: FormFeilds.containerImage(assetImage: 'assets/images/edit.png',height: 18,width: 18),
             ),
             IconButton(
-              onPressed: () {}, 
+              onPressed: (){
+                FormFeilds.mesgDelete(
+                  context, 
+                  'Are you sure to delete?',
+                  InkWell(
+                    onTap: () async {
+                      await FirebaseStorage.instance.refFromURL(categories['images']).delete();
+                      await getAllProductSale.doc(id).delete();
+                      Navigator.of(context).pop();
+                    },
+                    child: FormFeilds.buttonFormField(title: 'delete',colorButton: ColorTheme.primary),
+                  ),
+                  // FormFeilds.buttonFormField(title: 'cancel')
+                );
+              }, 
               icon: FormFeilds.containerImage(assetImage: 'assets/images/delete.png',height: 18,width: 18),
             ),
           ],
@@ -113,8 +170,13 @@ class _CategoriesWidgetState extends State<CategoriesWidget> {
       setState(() {
         salePRoducts.add(element.data());
       });
+        /* salePRoducts.add(element.id);
+        print('====================');
+        print(salePRoducts); */
     }
   }
+
+  
  /* Future<void> deleteData(index)async{
     var  postDat = FirebaseFirestore.instance.collection('categories').doc(postDat).delete();
     
@@ -124,5 +186,6 @@ class _CategoriesWidgetState extends State<CategoriesWidget> {
 }
 
 /* https://www.mhany-store.com/IMG/HOME/002.jpg
+  return Text("${getAllProductSale.doc().id}");
   
  */
