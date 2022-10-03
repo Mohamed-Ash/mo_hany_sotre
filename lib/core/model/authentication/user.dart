5e289554ebaaa6898bd73f1fb4c4c20b7bb0df35
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-03 13:42:12 +0200

    Create user.dart

---
+++ lib/core/model/authentication/user.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable{
+  final String id;
+  final String? email;
+  final String? name;
+  final String? photo;
+
+  const User({required this.id,this.email, this.name, this.photo,});
+  static const empty = User(id: '');
+  bool get isEmpty => this == User.empty;
+  bool get isNotEmpty => this != User.empty;
+  
+  @override
+  List<Object?> get props => [id,email,name,photo];
+
+}
