bc90f9e455b51469c32f3a0bae25fa41cd136178
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-03 13:40:37 +0200

    Update color_theme.dart

--- lib/core/theme/colors/color_theme.dart
+++ lib/core/theme/colors/color_theme.dart
@@ -10,7 +10,7 @@ class ColorTheme{
   static const Color darkAppBar                                = Color(0xff262a2d); 
   static const Color darkBackroundPage                         = Color(0xff111315);
   static const Color primary                                   = Color(0xff6610f2);
-  static const Color white                                     = Color(0xffFFFFFF);
+  static const Color wight                                     = Color(0xffFFFFFF);
   static const Color backroundInput                            = Color(0xff0f1a2b);
   static const Color titleTypePlay                             = Color(0xff303031);
   static const Color porder                                    = Color(0xff303031);
