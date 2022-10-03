f264aceb7f09d7a1f41a4399b9a026ff3e19ebc0
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-03 13:44:24 +0200

    Update add_item_shipping_page.dart

--- lib/admin/shipping/shipping_page/add_item_shipping_page.dart
+++ lib/admin/shipping/shipping_page/add_item_shipping_page.dart
@@ -1,12 +1,23 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:m_hany_store/admin/admin_interface.dart';
+import 'package:m_hany_store/admin/shipping/shipping_widget/add_item_shipping_widget.dart';
+import 'package:m_hany_store/core/theme/colors/color_theme.dart';
+import 'package:m_hany_store/core/theme/fonts/style.dart';
 
 class AddItemShippingPage extends AdminInterface{
   const AddItemShippingPage({super.key});
 
   @override
+  PreferredSizeWidget? get appBar => AppBar(
+    title: Text(
+      'Dashbord',
+      style: getBoldStyle(color: ColorTheme.wight,fontSize: 22),
+    ),
+  );
+
+  @override
   Widget buildBody(BuildContext context) {
-    return Column();
+    return const AddItemShippingWidget();
   }
 
 }
