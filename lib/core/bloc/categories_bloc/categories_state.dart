456ffb7c59a5cf59c9840c75dbd3c78afaa32307
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-03 13:42:44 +0200

    Create categories_state.dart

---
+++ lib/core/bloc/categories_bloc/categories_state.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: must_be_immutable
+
+part of 'categories_bloc.dart';
+
+abstract class CategoriesState extends Equatable {
+  const CategoriesState();
+  
+ 
+}
+
+class CategoriesInitial extends CategoriesState {
+
+  @override
+  List<Object?> get props => [];
+}
+
+class CategoriesLoadingState extends CategoriesState {
+  @override
+  List<Object?> get props => [];
+}
+
+class AddCategoriesState extends CategoriesState {
+  
+  @override
+  List<Object?> get props => [];
+}
+
+
+class CategoriesErrorState extends CategoriesState {
+  final String error;
+
+  const CategoriesErrorState(this.error);
+  
+  @override
+  List<Object?> get props => [error];
+}
+
+class GetCategoriesLoadedState extends CategoriesState {
+
+  List<CategoriesModel> categoriesModel = [];
+  
+  GetCategoriesLoadedState(this.categoriesModel);
+
+  @override
+  List<Object?> get props => [categoriesModel];
+}
+
+class GetCategoriesSthippngState extends CategoriesState {
+  List<CategoriesModel> categoriesModel = [];
+
+  GetCategoriesSthippngState(categoriesModel);
+  
+  @override
+  List<Object?> get props => [categoriesModel];
+}
