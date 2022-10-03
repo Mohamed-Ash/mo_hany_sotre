0a2dc17f0b836ce0f353226b32071d4fb361ebbc
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-03 13:44:03 +0200

    Update shipping_widget.dart

--- lib/admin/shipping/shipping_widget/shipping_widget.dart
+++ lib/admin/shipping/shipping_widget/shipping_widget.dart
@@ -1,7 +1,12 @@
 // ignore_for_file: avoid_print
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:m_hany_store/admin/shipping/shipping_page/edit_item_shipping_page.dart';
+import 'package:m_hany_store/admin/shipping/shipping_page/preview_item_shipping_page.dart';
+import 'package:m_hany_store/core/form_fields/button_form_feilds.dart';
+import 'package:m_hany_store/core/routes/string_route.dart';
 import 'package:m_hany_store/core/theme/colors/color_theme.dart';
 import 'package:m_hany_store/core/theme/fonts/style.dart';
 
@@ -16,8 +21,9 @@ class _ShippingWidgetState extends State<ShippingWidget> {
 
   List salePRoducts = [];
 
-  final Query getAllProductSale = FirebaseFirestore.instance.collection('categories').where("type", isEqualTo: 'shipping');
-  // final Query getAllProductSale = FirebaseFirestore.instance.collection('categories').where("title", isEqualTo: 'steam');
+  // var ref  = FirebaseFirestore.instance.collection('categories').doc().id; 
+  CollectionReference getAllProductSale = FirebaseFirestore.instance.collection('categories').doc('5144z0GZ5BA4m8riyX4D').collection('shipping');
+  // final Query getAllProductSale = FirebaseFirestore.instance.collection('categories').where("titlre", isEqualTo: 'steam');
     
   @override
   void initState() {
@@ -44,7 +50,7 @@ class _ShippingWidgetState extends State<ShippingWidget> {
                     shrinkWrap: true,
                     scrollDirection: Axis.vertical,
                     itemBuilder: (context ,index)=> buildItemProduct(
-                      categories: snapshot.data!.docs[index],
+                      shipping: snapshot.data!.docs[index],
                       context: context, 
                       // index: index, 
                       id: snapshot.data!.docs[index].id
@@ -53,98 +59,131 @@ class _ShippingWidgetState extends State<ShippingWidget> {
                       color: ColorTheme.porder,
                       thickness: 1,
                     ),
-                    itemCount: salePRoducts.length,
+                    itemCount: snapshot.data!.docs.length,
                   ),
                 );
               } else if(snapshot.connectionState == ConnectionState.waiting){
                 return const Center(child:  CircularProgressIndicator(),);
-              } else {
+              } else if(snapshot.connectionState == ConnectionState.none){
+                print('succsess');
+                return Center(child: Text('You do not publish eny shipping',style: getSemiBoldStyle(color: ColorTheme.wight,fontSize: 14,),));
+              }else {
                 return const Text('error 404');
               }
             },
           ),
           // const Spacer(),
-          /* const SizedBox(height: 22,),
+          const SizedBox(height: 22,),
           InkWell(
-            onTap: () => Navigator.pushNamed(context, addItemCategoriesPage),
+            onTap: () => Navigator.pushNamed(context, addItemShippingPage),
             child: FormFeilds.buttonFormField(
-              title: 'Add  new Categories',
+              title: 'Add new shipping',
               dPadding: false,
               heightButton: 40,
               colorButton: ColorTheme.primary
             ),
-          ), */
+          ),
         ],
       ),
     );
   }
 
  Widget buildItemProduct({
-   required BuildContext context,
+    required BuildContext context,
     // required int index,
     required String id,
-    required DocumentSnapshot categories,
- }){
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            width: 75,
-            height: 75,
-            decoration: BoxDecoration(
-              // color: Colors.white,
-              borderRadius: BorderRadius.circular(12),
-              image:  DecorationImage(
-                fit: BoxFit.contain,
-                image: NetworkImage('${categories['images']}')
+    required DocumentSnapshot shipping,
+  }){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          width: 75,
+          height: 75,
+          decoration: BoxDecoration(
+            // color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            image:  DecorationImage(
+              fit: BoxFit.contain,
+                image: NetworkImage(shipping['image'])
               ),
             ),
           ),
           
-          // const Spacer(),
+          const SizedBox(width: 18,),
           Flexible(
             // flex: 3,
             child: Text(
-              '${categories['name']}',
-              style: getSemiBoldStyle(color: ColorTheme.white,fontSize: 14,),
+              shipping['name'],
+              style: getSemiBoldStyle(color: ColorTheme.wight,fontSize: 14,),
               maxLines: 2,
               overflow: TextOverflow.ellipsis,
             ),
           ),
-          // const Spacer(),
+          const Spacer(),
           // const SizedBox(width: 12,),
-          /* Row(
+          Row(
             children: [ 
               IconButton(
-                onPressed: () =>Navigator.pushNamed(context, previewItemCategoriesPage) , 
+                onPressed: () => Navigator.push(
+                  context, MaterialPageRoute(
+                    builder: (context){
+                      return PreviewItemShippingPage(id, shipping);
+                    }
+                  ),
+                ),
                 icon: FormFeilds.containerImage(assetImage: 'assets/images/eye.png',height: 18,width: 18),
               ),
               IconButton(
-                onPressed: () => Navigator.pushNamed(context, editItemCategoriesPage),
+                // onPressed: () => Navigator.pushNamed(context, editItemCategoriesPage,arguments: id),
+                onPressed: () => Navigator.push(
+                  context, MaterialPageRoute(
+                    builder: (context){
+                      return  EditItemShippingPage(shipping: shipping,id: id,);
+                    }
+                  ),
+                ),
                 icon: FormFeilds.containerImage(assetImage: 'assets/images/edit.png',height: 18,width: 18),
               ),
               IconButton(
-                onPressed: () {}, 
+                onPressed: (){
+                  FormFeilds.mesgDelete(
+                    context, 
+                    'Are you sure to delete?',
+                    InkWell(
+                      onTap: () async {
+                        await FirebaseStorage.instance.refFromURL(shipping['image']).delete();
+                        await getAllProductSale.doc(id).delete();
+                        // ignore: use_build_context_synchronously
+                        Navigator.of(context).pop();
+                      },
+                      child: FormFeilds.buttonFormField(title: 'delete',colorButton: ColorTheme.primary),
+                    ),
+                  );
+                }, 
                 icon: FormFeilds.containerImage(assetImage: 'assets/images/delete.png',height: 18,width: 18),
               ),
             ],
-          ), */
-        ],
-      ),
+          ),
+      ],
     );
   }
 
 
   Future<void> getCategories() async {
+ 
+
+
     QuerySnapshot responseBody  = await getAllProductSale.get();
     for(var element in responseBody.docs){
       setState(() {
         salePRoducts.add(element.data());
       });
-       print(element.data());
+        /* salePRoducts.add(element.id);
+        print('====================');
+        print(salePRoducts); */
     }
   }
+
   
 }
