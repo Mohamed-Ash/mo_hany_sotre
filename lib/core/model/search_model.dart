01838d0c10f3aa197cbdabe5b83743d9fc1e158a
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-09 18:54:49 +0200

    Create search_model.dart

---
+++ lib/core/model/search_model.dart
@@ -0,0 +1,64 @@
+
+
+class SearchModel{
+  final int? id; 
+  final String? idDoc; ///document id
+  final String name;
+  final int colorPlatform;
+  final int colorTextPlatform;
+  final String image;
+  final String region;
+  final String price;
+  final String platform;
+  final String? type;
+  final String? createdAt;
+  final String? updatedAt;
+  // sale ends
+
+  SearchModel({
+    this.id,
+    this.idDoc,
+    required this.name,
+    required this.image,
+    required this.region,
+    required this.price,
+    required this.platform,
+    required this.colorPlatform,
+    required this.colorTextPlatform,
+    this.type,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  factory SearchModel.formJson(Map<String,dynamic>json){
+    return SearchModel(
+      id: json['id'],
+      idDoc: json['id_doc'],
+      name: json['name'], 
+      image: json['image'], 
+      region: json['region'], 
+      price: json['price'],
+      platform: json['plat_form'],
+      colorPlatform: json['color_plat_form'],
+      colorTextPlatform: json['color_text_plat_form'],
+      type: json['type'],
+      createdAt: json['created_at'],
+      updatedAt: json['updated_at'],
+    );
+  }
+
+  Map<String,dynamic> toJson()=>{
+    "id": id,
+    "id_doc": idDoc,
+    "color_text_plat_form":colorTextPlatform,
+    "color_plat_form":colorPlatform,
+    "name": name,
+    "image": image,
+    "region": region,
+    "price": price,
+    "plat_form": platform,
+    "type": type,
+    "created_at": createdAt,
+    "updated_at": updatedAt,
+  };
+}
