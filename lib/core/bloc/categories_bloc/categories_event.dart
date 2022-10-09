d38c411b0cbee87264f426ccf3bc5b49ba727e14
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-09 18:55:58 +0200

    Update categories_event.dart

--- lib/core/bloc/categories_bloc/categories_event.dart
+++ lib/core/bloc/categories_bloc/categories_event.dart
@@ -3,22 +3,22 @@ part of 'categories_bloc.dart';
 abstract class CategoriesEvent extends Equatable {}
 
 class AddCategoriesEvent extends CategoriesEvent{
-  final int  id;
-  final String idDoc;
-  final String  name;
-  final String  image;
-  final String  type;
-  final String  createdAt;
-  final String  updatedAt;
+  final int? id;
+  final String? idDoc;
+  final String?  name;
+  final String?  image;
+  final String?  type;
+  final String? createdAt;
+  final String?  updatedAt;
 
   AddCategoriesEvent({
-    required this.id,
-    required this.idDoc,
-    required this.name,
-    required this.image, 
-    required this.type, 
-    required this.updatedAt, 
-    required this.createdAt
+    this.id,
+    this.idDoc,
+    this.name,
+    this.image, 
+    this.type, 
+    this.updatedAt, 
+    this.createdAt
   });
   
   @override
@@ -30,18 +30,26 @@ class AddCategoriesEvent extends CategoriesEvent{
     updatedAt,
   ];
 }
+class DeleteCategoriesEvent extends CategoriesEvent{
+  final int  id;
+  final String idDoc;
 
+  DeleteCategoriesEvent({
+    required this.id,
+    required this.idDoc,
+  });
+
+    @override
+  List<Object?> get props => [
+    id,
+    idDoc,
+  ];
+}
 class GetAllCategoriesEvent extends CategoriesEvent{
   @override
   List<Object?> get props => [];
 }
 class GetCategoriesLoadingEvent extends CategoriesEvent{
-  @override
-  List<Object?> get props => [];
-}
-
-class GetCategoriesShippingEvent extends CategoriesEvent{
-
   @override
   List<Object?> get props => [];
 }
