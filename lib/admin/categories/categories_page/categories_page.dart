211f430b6ab4aa7a824f77adcb5cc388ff3c972e
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-10 14:01:16 +0200

    Update categories_page.dart

--- lib/admin/categories/categories_page/categories_page.dart
+++ lib/admin/categories/categories_page/categories_page.dart
@@ -4,6 +4,7 @@ import 'package:m_hany_store/admin/admin_interface.dart';
 import 'package:m_hany_store/admin/categories/categories_widget/categories_widget.dart';
 import 'package:m_hany_store/core/bloc/categories_bloc/categories_bloc.dart';
 import 'package:m_hany_store/core/repositories/admin/categories_ropository.dart';
+import 'package:m_hany_store/core/routes/string_route.dart';
 import 'package:m_hany_store/core/theme/colors/color_theme.dart';
 import 'package:m_hany_store/core/theme/fonts/style.dart';
 
@@ -12,14 +13,15 @@ class CategoriePage extends AdminInterface{
   
   @override
   PreferredSizeWidget? appBar(BuildContext context) => AppBar(
+    leading: IconButton(
+      onPressed: () => Navigator.pushNamedAndRemoveUntil(context, appPageLayout, (route) => false),
+      icon: const Icon(Icons.arrow_back_ios_new_rounded) 
+    ),
     title: Text(
-      'Dashbord',
+      'Categories',
       style: getBoldStyle(color: ColorTheme.wight,fontSize: 22),
     ),
   );
-
-  /* @override
-  Widget? get buildDrawer =>  const AdminDrawer(); */
 
   @override
   Widget buildBody(BuildContext context){
