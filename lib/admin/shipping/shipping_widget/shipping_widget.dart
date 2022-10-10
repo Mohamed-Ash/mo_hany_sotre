c140b94c6a188764e9f4403ba1a4496bf6ee1138
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-10 13:56:46 +0200

    Update shipping_widget.dart

--- lib/admin/shipping/shipping_widget/shipping_widget.dart
+++ lib/admin/shipping/shipping_widget/shipping_widget.dart
@@ -189,7 +189,7 @@ class _ShippingWidgetState extends State<ShippingWidget> {
                 ),
                 IconButton(
                   onPressed: (){
-                    FormFeilds.mesgDelete(
+                    FormFeilds.deleteMessage(
                       context, 
                       'Are you sure to delete?',
                       InkWell(
