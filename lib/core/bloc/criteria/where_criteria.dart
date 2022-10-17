9b3e73e7546ae29c1d432dd77fbfe27dbceb805e
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-17 05:32:51 +0200

    creat where interface to filter data

---
+++ lib/core/bloc/criteria/where_criteria.dart
@@ -0,0 +1,30 @@
+
+class WhereCriteria {
+  final Object field;
+  final Object? isEqualTo;
+  final Object? isNotEqualTo;
+  final Object? isLessThan;
+  final Object? isLessThanOrEqualTo;
+  final Object? isGreaterThan;
+  final Object? isGreaterThanOrEqualTo;
+  final Object? arrayContains;
+  final List<Object?>? arrayContainsAny;
+  final List<Object?>? whereIn;
+  final List<Object?>? whereNotIn;
+  final bool? isNull;
+
+  WhereCriteria({
+    required this.field,
+    this.isEqualTo,
+    this.isNotEqualTo,
+    this.isLessThan,
+    this.isLessThanOrEqualTo,
+    this.isGreaterThan,
+    this.isGreaterThanOrEqualTo,
+    this.arrayContains,
+    this.arrayContainsAny,
+    this.whereIn,
+    this.whereNotIn,
+    this.isNull,
+  });
+}
