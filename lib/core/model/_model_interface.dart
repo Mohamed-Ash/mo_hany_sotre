bc0b8960c09434140bc4c9d72655aee6e27c17a9
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-17 05:30:20 +0200

    Update _model_interface.dart

--- lib/core/model/_model_interface.dart
+++ lib/core/model/_model_interface.dart
@@ -1,10 +1,24 @@
-/* class ModelClass {
+import 'package:m_hany_store/core/model/category_model.dart';
+import 'package:m_hany_store/core/model/item_model.dart';
+
+class ModelClass {
   final String collection;
-  ModelClass({
+  final Function factory;
+
+  const ModelClass({
     required this.collection,
+    required this.factory,
   });
 }
 
 class ModelInterface {
-  ModelClass? getSel
-} */
+
+  static ModelClass? getModelClass(Type type) {
+    return _models.entries.firstWhere((element) => element.key == type).value;
+  }
+
+  static const Map<Type, ModelClass> _models = {
+    ItemModel:     ModelClass(collection: 'items', factory: ItemModel.formJson),
+    CategoryModel: ModelClass(collection: 'categories', factory: CategoryModel.fromJson),
+  };
+}
