e07ec0bdc2adfdd1a1a16d0eb783ae9070209de3
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-17 05:27:30 +0200

    add bloc and fix  image and  add filter to get data  offer

--- lib/user/sale/sale_page/sale_page.dart
+++ lib/user/sale/sale_page/sale_page.dart
@@ -1,114 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:m_hany_store/core/bloc/bloc/api_data_bloc.dart';
+import 'package:m_hany_store/core/model/item_model.dart';
 import 'package:m_hany_store/user/sale/sale_widget/sale_widget.dart';
 import 'package:m_hany_store/user/user_interface.dart';
 
+// ignore: must_be_immutable
 class SalePage extends UserInterface{
-  const SalePage({super.key});
+  late ApiDataBloc<ItemModel> itemBloc;
+  ItemModel? itemModel;
+  
+  SalePage({super.key}) {
+    itemBloc = ApiDataBloc<ItemModel>()..add( const IndexDataEvent(),);
+  }
 
   @override
   Widget buildBody(BuildContext context) {
-    return  const SaleWidget();
-
-    /* return  SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(8,22,8,8),
-        child: ListView.builder(
-          itemBuilder: (context,index){
-            return buldItemSale(context);
-          }), */
-        /* child: Column(
-          children: [
-            InkWell(
-              onTap: () => Navigator.pushNamed(context, previewSalePage),
-              child: Container(
-                width: double.infinity,
-                height: 420,
-                decoration: BoxDecoration(
-                color: ColorTheme.darkAppBar,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 333,
-                      decoration: BoxDecoration(
-                        borderRadius:BorderRadius.circular(8),
-                        //  color: Colors.black,
-                        image: const DecorationImage(
-                          fit: BoxFit.fill,
-                          image: NetworkImage(
-                            // 'https://wallpaperaccess.com/full/7070020.jpg',
-                            'https://media-rockstargames-com.akamaized.net/rockstargames-newsite/img/global/games/fob/1280/V.jpg'
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(15,15,0,15),
-                      child: Text(
-                        'FIFA 22',
-                        style: getSemiBoldStyle(color: ColorTheme.white, fontSize: 16),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(15,0,0,5),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [ 
-                          Container(
-                            width: 50,
-                            height: 25,
-                            decoration: BoxDecoration(
-                              color: ColorTheme.primary,
-                              borderRadius: BorderRadius.circular(4),
-                            ),
-                            child: Center(
-                              child: Text(
-                                '-20%',
-                                style: getSemiBoldStyle(color: ColorTheme.white, fontSize: 16),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 12,
-                          ),
-                           Text.rich(
-                            TextSpan(
-                              text: '250 LE',
-                              style: TextStyle(
-                                decoration: TextDecoration.combine(
-                                  [
-                                    TextDecoration.lineThrough,
-                                    TextDecoration.lineThrough,
-                                  ],
-                                ),
-                                fontFamily: FontsTheme.fontFamily,
-                                color: Colors.grey,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 12,
-                          ),
-                          Text(
-                            '119.99 LE',
-                            style: getBoldStyle(color: ColorTheme.white,fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ), */
-    //   ),
-    // );
+    return SaleWidget(itemBloc: itemBloc,);
   }
-
-  
 }
--- lib/user/sale/sale_widget/sale_widget.dart
+++ lib/user/sale/sale_widget/sale_widget.dart
@@ -1,14 +1,19 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:m_hany_store/core/bloc/bloc/api_data_bloc.dart';
+import 'package:m_hany_store/core/bloc/criteria/where_criteria.dart';
+import 'package:m_hany_store/core/model/item_model.dart';
 import 'package:m_hany_store/core/routes/string_route.dart';
 import 'package:m_hany_store/core/theme/colors/color_theme.dart';
 import 'package:m_hany_store/core/theme/fonts/font_theme.dart';
 import 'package:m_hany_store/core/theme/fonts/style.dart';
 
+// ignore: must_be_immutable
 class SaleWidget extends StatefulWidget {
- 
+ late ApiDataBloc<ItemModel> itemBloc;
+ ItemModel? itemModel;
   
-  const SaleWidget({Key? key}) : super(key: key);
+  SaleWidget({Key? key,required this.itemBloc}) : super(key: key);
   
   @override
   State<SaleWidget> createState() => Ssale_widgWState();
@@ -16,14 +21,13 @@ class SaleWidget extends StatefulWidget {
 
 // ignore: camel_case_types
 class Ssale_widgWState extends State<SaleWidget> {
-  List salePRoducts = [];
-
-  CollectionReference getAllProductSale = FirebaseFirestore.instance.collection('discounts product');
 
  @override
   void initState() {
-    getProducts();
     super.initState();
+   widget.itemBloc = ApiDataBloc<ItemModel>()..add(
+      IndexDataEvent(
+        where:  WhereCriteria(field: 'test' ,isEqualTo: 'true'),));
   }
   
 
@@ -31,21 +35,30 @@ class Ssale_widgWState extends State<SaleWidget> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(8,22,8,8),
-      child: ListView.separated( 
-        /* prototypeItem: const SizedBox(
-          height: 12,
-        ),  */
-
-        itemCount: salePRoducts.length,
-        itemBuilder: (context,index){
-          return buldItemSale(context,index);
-        }, 
-        separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 22,)
-        
+      child: BlocBuilder(
+        bloc: widget.itemBloc,
+        builder: (context, state) {
+          if (state is DataLoadedState) {
+            return ListView.separated( 
+              itemCount: state.data.length,
+              itemBuilder: (context,index){
+                return buldItemSale(
+                  context: context,
+                  itemModel: state.data[index]
+                );
+              }, 
+              separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 22,)
+            );
+          }else if(state is DataLoadingState){
+            return  const Center(child: CircularProgressIndicator(color: ColorTheme.primary),);
+          }else{
+            return Text('error 404',style: getSemiBoldStyle(color: ColorTheme.wight,fontSize: 14,),);
+          }
+        }
       ),
     );
   }
-  getProducts()async {
+/*   getProducts()async {
     QuerySnapshot responseBody  = await getAllProductSale.get();
     for(var element in responseBody.docs){
       setState(() {
@@ -53,8 +66,11 @@ class Ssale_widgWState extends State<SaleWidget> {
       });
     }
   }
-
-  buldItemSale(context,index){
+ */
+  buldItemSale({
+    required BuildContext context,
+    required ItemModel itemModel,
+  }){
     return InkWell(
       onTap: () => Navigator.pushNamed(context, showSalePage),
       child: Container(
@@ -77,7 +93,7 @@ class Ssale_widgWState extends State<SaleWidget> {
                   fit: BoxFit.fill,
                   image: NetworkImage(
                     // 'https://wallpaperaccess.com/full/7070020.jpg',
-                    '${salePRoducts[index]["image"]}'
+                    itemModel.image
                   ),
                 ),
               ),
@@ -85,7 +101,7 @@ class Ssale_widgWState extends State<SaleWidget> {
             Padding(
               padding: const EdgeInsets.fromLTRB(15,15,0,15),
               child: Text(
-                '${salePRoducts[index]["name"]}',
+                itemModel.name,
                 style: getSemiBoldStyle(color: ColorTheme.wight, fontSize: 16),
               ),
             ),
@@ -113,7 +129,7 @@ class Ssale_widgWState extends State<SaleWidget> {
                   ),
                     Text.rich(
                     TextSpan(
-                      text: '${salePRoducts[index]["old price"]} LE',
+                      text: '${itemModel.price} old price LE',
                       style: TextStyle(
                         decoration: TextDecoration.combine(
                           [
@@ -131,7 +147,7 @@ class Ssale_widgWState extends State<SaleWidget> {
                     width: 12,
                   ),
                   Text(
-                    '${salePRoducts[index]["new price"]} LE',
+                    '${itemModel.price} new price LE',
                     style: getBoldStyle(color: ColorTheme.wight,fontSize: 16),
                   ),
                 ],
