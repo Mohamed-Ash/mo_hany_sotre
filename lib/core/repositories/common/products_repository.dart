7acd87b507eaced0bea01f53c128e8f1208e2989
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-17 05:33:36 +0200

    Update products_repository.dart

--- lib/core/repositories/common/products_repository.dart
+++ lib/core/repositories/common/products_repository.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+/* import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:m_hany_store/core/model/product_model.dart';
 
@@ -17,4 +17,4 @@ class ProductReposiory{
       return null;
     }
   }
-}
+} */
