4f8c6cee3c3514a879eecc6ffcc1d736e09e3123
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-31 13:41:30 +0200

    Update button_form_feilds.dart

--- lib/core/form_fields/button_form_feilds.dart
+++ lib/core/form_fields/button_form_feilds.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:m_hany_store/core/theme/colors/color_theme.dart';
 import 'package:m_hany_store/core/theme/fonts/font_theme.dart';
 import 'package:m_hany_store/core/theme/fonts/style.dart';
@@ -65,7 +66,7 @@ class FormFeilds{
   }
 
 
-  static TextFormField textField({
+  static TextFormField textFormField({
     required TextEditingController controller, 
     required TextInputType keyboardType,
     bool obscureText = false,
@@ -76,6 +77,7 @@ class FormFeilds{
     InputBorder border = InputBorder.none, 
     String? Function(String?)? validator,
     void Function(String)? onChanged,
+    Color coloInputText = Colors.white,
     EdgeInsets contentPadding = const EdgeInsets.all(12),
     double cursorHeight = 22, 
     Widget? suffixIcon,
@@ -94,13 +96,68 @@ class FormFeilds{
       controller: controller,
       obscureText: obscureText,
       // keyboardAppearance: Brightness.light,
-      style: const TextStyle(
-        color: Colors.white,
+      style: TextStyle(
+        color: coloInputText,
         decoration: TextDecoration.none,
         fontFamily: FontsTheme.fontFamily,
         decorationThickness: 0,
       ),
       validator: validator,
+      onChanged: onChanged,
+      keyboardAppearance: Brightness.dark,
+      decoration: InputDecoration(
+        // contentPadding: const EdgeInsets.fromLTRB(12, 22, 0, -10),
+        alignLabelWithHint: true,
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        contentPadding: contentPadding,
+        errorStyle: getRegulerStyle(color: Colors.red,fontSize: 12),
+        hintText: hintText,
+        suffixIcon: suffixIcon,
+        prefixIcon: prefixIcon,
+        prefixIconColor: ColorTheme.primary,
+        hintStyle: getBoldStyle(color: hintTextColor, dDecoration: TextDecoration.none,),
+        border: InputBorder.none,
+        focusedBorder: InputBorder.none,
+        enabledBorder: InputBorder.none,
+        errorBorder: InputBorder.none,
+        disabledBorder: InputBorder.none,
+      ),
+    );
+  }
+  static TextField textField({
+    required TextEditingController controller, 
+    required TextInputType keyboardType,
+    bool obscureText = false,
+    // required TextStyle hintStyle,
+    required String hintText,
+    Color cursorColor = ColorTheme.primary,
+    Color hintTextColor = ColorTheme.hintText,
+    InputBorder border = InputBorder.none, 
+    String? Function(String?)? validator,
+    void Function(String)? onChanged,
+    Color coloInputText = Colors.white,
+    EdgeInsets contentPadding = const EdgeInsets.all(12),
+    double cursorHeight = 22, 
+    Widget? suffixIcon,
+    Widget? prefixIcon,
+    String? initialValue
+    }){
+    return TextField(
+      textAlignVertical: TextAlignVertical.center,
+      keyboardType: keyboardType,
+      cursorColor: cursorColor,
+      cursorHeight: cursorHeight, 
+      controller: controller,
+      maxLines: null,
+      obscureText: obscureText,
+      // keyboardAppearance: Brightness.light,
+      style: TextStyle(
+        
+        color: coloInputText,
+        decoration: TextDecoration.none,
+        fontFamily: FontsTheme.fontFamily,
+        decorationThickness: 0,
+      ),
       onChanged: onChanged,
       keyboardAppearance: Brightness.dark,
       decoration: InputDecoration(
@@ -121,6 +178,7 @@ class FormFeilds{
       ),
     );
   }
+
   static Row rowTextIcon({
      String?  firstIconImage,
     // required String  iconImage,
@@ -216,7 +274,7 @@ class FormFeilds{
   }){
     return Container(
     width: double.infinity,
-    height: 50,
+    height: 60,
     decoration:   BoxDecoration(
       borderRadius: BorderRadius.circular(4),
       border: const Border(
@@ -243,9 +301,10 @@ class FormFeilds{
       leading: FormFeilds.containerImage(assetImage: assetImage,height: 28,width: 28),
         title: Text(
           title,
-          style: getSemiBoldStyle(color: ColorTheme.hintText),
+          style: getSemiBoldStyle(color: ColorTheme.wight),
         ),
         horizontalTitleGap: 33,
+        minVerticalPadding: 0,
       ),
     );
   }
@@ -253,21 +312,45 @@ class FormFeilds{
   static showMyDialog({
    required final BuildContext context,
     required final String message,
+    String selectMessage = '',
+    Color selectMessageColor = ColorTheme.wight,
     List<Widget>? actions,
-    bool tabButtonRequired = false,
+    bool tabButton = false,
+    String image = 'assets/images/uploaded_done.png',
+    bool isImage = false,
+    double heightImage = 35,
+    double widthImage = 35,
   }){
     return showDialog(
       context: context,
-      barrierDismissible: tabButtonRequired, /// user must tap button!
+      barrierDismissible: tabButton, /// user must tap button!
       // barrierColor: ColorTheme.hintText,
       builder: (BuildContext context) {
         return AlertDialog(
-          icon: FormFeilds.containerImage(assetImage: 'assets/images/uploaded_done.png'),
+          icon: isImage == true ? FormFeilds.containerImage(assetImage: image,height: heightImage,width:widthImage ) : null,
           backgroundColor: ColorTheme.darkAppBar,
           actions: actions,
           alignment: Alignment.center,
           // contentPadding: EdgeInsets.fromLTRB(left, top, right, bottom) ,
-          title: Text(message,style: getSemiBoldStyle(color: ColorTheme.wight,fontSize: 14)),
+          title: Column(
+            children: [
+              Text(message,style: getSemiBoldStyle(color: ColorTheme.wight,fontSize: 14)),
+              const SizedBox(
+                height: 15,
+              ),
+              SelectableText(
+                selectMessage,
+                style: getBoldStyle(color: Colors.blue,fontSize: 16),
+                toolbarOptions: const ToolbarOptions(copy: true,cut: false,paste: false,selectAll: false),
+                // onSelectionChanged: (selection, cause) => Text('Copyed'),
+                autofocus: true,
+                onTap: () {
+                  Clipboard.setData(const ClipboardData(text: "01068693963"),
+                  );
+                },
+              ) 
+            ],
+          ),
           // content: Text(message,style: getSemiBoldStyle(color: ColorTheme.wight,fontSize: 14)),
         );
       },
