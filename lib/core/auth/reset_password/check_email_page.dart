2140fbc2eacd2d7572636ffad6cf06f8dc1a9500
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-31 13:37:55 +0200

    Update check_email_page.dart

--- lib/core/auth/reset_password/check_email_page.dart
+++ lib/core/auth/reset_password/check_email_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:m_hany_store/core/form_fields/button_form_feilds.dart';
 import 'package:m_hany_store/core/routes/string_route.dart';
 import 'package:m_hany_store/core/theme/colors/color_theme.dart';
-import 'package:m_hany_store/core/theme/fonts/font_theme.dart';
 import 'package:m_hany_store/core/theme/fonts/style.dart';
 import 'package:m_hany_store/user/user_interface.dart';
 
