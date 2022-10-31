daeb53a10cc49e927fdd9fc41b1f089c1690baf1
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-31 13:39:28 +0200

    create admin panel

---
+++ lib/admin/admin_panel/admin_panel_page/admin_panel_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:m_hany_store/admin/admin_interface.dart';
+
+class AdminPanelPage extends AdminInterface{
+  const AdminPanelPage({super.key});
+
+  @override
+  Widget buildBody(BuildContext context) {
+    return Column();
+  }
+}
---
+++ lib/admin/admin_panel/admin_panel_widget/admin_panel_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminPanelWidget extends StatefulWidget {
+  const AdminPanelWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AdminPanelWidget> createState() => _AdminPanelWidgetState();
+}
+
+class _AdminPanelWidgetState extends State<AdminPanelWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
