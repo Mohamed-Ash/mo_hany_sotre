650caf84b2fb453cf048e51a94ce31194b4b7820
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-10-31 13:41:33 +0200

    Update next_id_helper.dart

--- lib/core/helper/next_id_helper.dart
+++ lib/core/helper/next_id_helper.dart
@@ -2,13 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class NextIdHelper{
 
-  static Future<int> getNextId( String collectionName)async{
-    final fireStore = await FirebaseFirestore.instance.collection(collectionName).get();
-    return fireStore.docs.length + 1;
+  static Future<String> getNextId( String collectionName)async{
+    final fireStore = await FirebaseFirestore.instance.collection(collectionName).doc().get();
+    return fireStore.id;
   }
-
-  /* static Future<int,s> getNextIdDoc(String collectionName, String idDocs)async{
-    final fireStore = await FirebaseFirestore.instance.collection(collectionName).doc(idDocs).get();
-    return fireStore.docs.length + 1;
-  } */
 }
