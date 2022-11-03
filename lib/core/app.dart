0ab70bccc233f514edd12afe66689ad177fa842e
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-11-03 16:40:21 +0200

    fix import

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:m_hany_store/core/routes/route.dart';
 import 'package:m_hany_store/core/routes/string_route.dart';
-import 'package:m_hany_store/core/theme/colors/color_theme.dart';
 import 'package:m_hany_store/core/theme/theme_manager.dart';
 
 class App extends StatelessWidget{
