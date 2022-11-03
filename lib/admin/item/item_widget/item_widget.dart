cc912bad15dad556cdf41aabdf0c5f84317be519
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-11-03 16:41:58 +0200

    Update item_widget.dart

--- lib/admin/item/item_widget/item_widget.dart
+++ lib/admin/item/item_widget/item_widget.dart
@@ -2,9 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:m_hany_store/admin/item/item_page/add_item_page.dart';
 import 'package:m_hany_store/admin/item/item_page/edit_item_item_page.dart';
-import 'package:m_hany_store/admin/item/item_page/preview_item_page.dart';
 import 'package:m_hany_store/core/bloc/bloc/api_data_bloc.dart';
 import 'package:m_hany_store/core/form_fields/button_form_feilds.dart';
 import 'package:m_hany_store/core/model/category_model.dart';
@@ -29,13 +27,6 @@ class ShippingWidget extends StatefulWidget {
 }
  
 class _ShippingWidgetState extends State<ShippingWidget> {
- /*  @override
-  void initState() {
-    super.initState();
-    test();
-  }
- */
-  
 
   @override
   Widget build(BuildContext context){
@@ -59,8 +50,7 @@ class _ShippingWidgetState extends State<ShippingWidget> {
                     ),
                   );
                 }else{
-                  return SizedBox(
-                    height: 555,
+                  return Expanded(
                     child: SingleChildScrollView(
                       physics: const ScrollPhysics(),
                       child: ListView.separated(
@@ -89,17 +79,7 @@ class _ShippingWidgetState extends State<ShippingWidget> {
           const SizedBox(height: 22,),
           InkWell(
             onTap: () {
-              Navigator.push(
-                  context,MaterialPageRoute(
-                  builder: (context){
-                    // return AddItemShippingPage(categoriesModel: widget.categoriesModel,);
-                    return AddItemShippingPage(
-                      categoriesModel: widget.categoriesModel,
-                    );
-
-                  }
-                ),
-              );
+              Navigator.pushReplacementNamed(context,addItemPage,arguments: widget.categoriesModel);
             },
             child: FormFeilds.buttonFormField(
               title: 'Add new item',
@@ -113,16 +93,6 @@ class _ShippingWidgetState extends State<ShippingWidget> {
       ),
     );
   }
-  
-/*   test(){
-    if (widget.categoriesModel.id == 2) {
-      widget.itemBloc.add(DeleteDataEvent(id: widget.categoriesModel.id, files: const ['image']));
-      print('deeeleeet');
-      print('==========');
-    }else{
-
-    }
-  } */
   
   Widget buildItemProduct({
     required BuildContext context,
@@ -143,96 +113,72 @@ class _ShippingWidgetState extends State<ShippingWidget> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const SizedBox(
-              width: 10,
+        const SizedBox(
+            width: 10,
+          ),
+          SizedBox(
+            width: 75,
+            height: 75,
+            child: PhysicalModel(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              color: Colors.black,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(8),
+              child: itemModel.image == null||  itemModel.image.isNotEmpty? FadeInImage.assetNetwork(
+                placeholder: 'assets/icons/lloading.gif',
+                image: itemModel.image,
+                fit: BoxFit.fill,
+                placeholderFit: BoxFit.fill,
+              ) : Image.asset('assets/images/no_image_available.jpg'),
             ),
-            SizedBox(
-              width: 75,
-              height: 75,
-              child: PhysicalModel(
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                color: Colors.black,
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(8),
-                child: itemModel.image.isNotEmpty? FadeInImage.assetNetwork(
-                  placeholder: 'assets/icons/lloading.gif',
-                  image: itemModel.image,
-                  fit: BoxFit.fill,
-                  placeholderFit: BoxFit.contain,
-                ) : Image.asset('assets/images/no_image_available.jpg'),
+          ),
+          const SizedBox(width: 18,),
+          Flexible(
+            fit: FlexFit.tight,
+            flex: 3,
+            child: Text(
+              itemModel.name,
+              style: getSemiBoldStyle(color: ColorTheme.wight,fontSize: 13,),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          const Spacer(),
+          Row(
+            children: [ 
+              IconButton(
+                onPressed: () => Navigator.pushReplacementNamed(context, previewItemPage, arguments: itemModel),
+                icon: FormFeilds.containerImage(assetImage: 'assets/images/eye.png',height: 18,width: 18),
               ),
-            ),
-            const SizedBox(width: 18,),
-            Flexible(
-              fit: FlexFit.tight,
-              flex: 3,
-              child: Text(
-                itemModel.name,
-                style: getSemiBoldStyle(color: ColorTheme.wight,fontSize: 13,),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
+              IconButton(
+                onPressed: () => Navigator.pushReplacement(
+                  context, MaterialPageRoute(
+                    builder: (context){
+                      return  EditItemPage(itemModel: itemModel,categoriesModel: widget.categoriesModel,);
+                    }
+                  ),
+                ),
+                icon: FormFeilds.containerImage(assetImage: 'assets/images/edit.png',height: 18,width: 18),
               ),
-            ),
-            const Spacer(),
-            Row(
-              children: [ 
-                IconButton(
-                  onPressed: () => Navigator.push(
-                    context, MaterialPageRoute(
-                      builder: (context){
-                        return PreviewItemShippingPage(shippingModel: itemModel,);
-                      }
+              IconButton(
+                onPressed: (){
+                  FormFeilds.deleteMessage(
+                    context, 
+                    'Are you sure to delete?',
+                    InkWell(
+                      onTap: () async {
+                          Navigator.of(context).pushReplacementNamed(itemPage, arguments: widget.categoriesModel);
+                        widget.itemBloc.add(DeleteDataEvent(id: itemModel.id, files: const ['image']));
+                        // Navigator.push(context, MaterialPageRoute(builder: (_)=> ShippingPage(categoriesModel: widget.categoriesModel,)));
+                      },
+                      child: FormFeilds.buttonFormField(title: 'delete',colorButton: ColorTheme.primary),
                     ),
-                  ),
-                  icon: FormFeilds.containerImage(assetImage: 'assets/images/eye.png',height: 18,width: 18),
-                ),
-                IconButton(
-                  onPressed: () => Navigator.push(
-                    context, MaterialPageRoute(
-                      builder: (context){
-                        return  EditItemShippingPage(itemModel: itemModel,categoriesModel: widget.categoriesModel,);
-                      }
-                    ),
-                  ),
-                  icon: FormFeilds.containerImage(assetImage: 'assets/images/edit.png',height: 18,width: 18),
-                ),
-                IconButton(
-                  onPressed: (){
-                   /*  FormFeilds.deleteMessage(
-                      context, 
-                      'Are you sure to delete?',
-                      InkWell(
-                        onTap: () async {
-                          widget.shippingRepository.deleteShippingByCategory(
-                            collectionIdDoc:widget.categoriesModel.idDoc,
-                            idDoc: shipping.idDoc,
-                            type: widget.categoriesModel.type,
-                            refFromURL: shipping.image
-                          );
-                          widget.shippingRepository.deleteSearchByCategory(idDoc:  shipping.idDoc,);
-                          Navigator.of(context).pop();
-                        },
-                        child: FormFeilds.buttonFormField(title: 'delete',colorButton: ColorTheme.primary),
-                      ),
-                    ); */
-                    FormFeilds.deleteMessage(
-                        context, 
-                        'Are you sure to delete?',
-                        InkWell(
-                        onTap: () async {
-                           Navigator.of(context).pushReplacementNamed(shippingPage, arguments: widget.categoriesModel);
-                          widget.itemBloc.add(DeleteDataEvent(id: itemModel.id, files: const ['image']));
-                          // Navigator.push(context, MaterialPageRoute(builder: (_)=> ShippingPage(categoriesModel: widget.categoriesModel,)));
-                        },
-                        child: FormFeilds.buttonFormField(title: 'delete',colorButton: ColorTheme.primary),
-                      ),
-                              // Navigator.push(context, MaterialPageRoute(builder: (_)=> ShippingPage(categoriesModel: widget.categoriesModel,)));
-                      );
-                  }, 
-                  icon: FormFeilds.containerImage(assetImage: 'assets/images/delete.png',height: 18,width: 18),
-                ),
-              ],
-            ),
+                  );
+                }, 
+                icon: FormFeilds.containerImage(assetImage: 'assets/images/delete.png',height: 18,width: 18),
+              ),
+            ],
+          ),
         ],
       ),
     );
