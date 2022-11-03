51536bfa5996d35f23c358ae8b4519017053efa7
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-11-03 16:42:58 +0200

    Update categories_page.dart

--- lib/admin/categories/categories_page/categories_page.dart
+++ lib/admin/categories/categories_page/categories_page.dart
@@ -3,6 +3,7 @@ import 'package:m_hany_store/admin/admin_interface.dart';
 import 'package:m_hany_store/admin/categories/categories_widget/categories_widget.dart';
 import 'package:m_hany_store/core/bloc/bloc/api_data_bloc.dart';
 import 'package:m_hany_store/core/model/category_model.dart';
+import 'package:m_hany_store/core/model/item_model.dart';
 import 'package:m_hany_store/core/routes/string_route.dart';
 import 'package:m_hany_store/core/theme/colors/color_theme.dart';
 import 'package:m_hany_store/core/theme/fonts/style.dart';
@@ -10,17 +11,20 @@ import 'package:m_hany_store/core/theme/fonts/style.dart';
 
 // ignore: must_be_immutable
 class CategoriePage extends AdminInterface{
-
+  ItemModel? itemModel;
   late ApiDataBloc<CategoryModel> categoryBloc;
   
   CategoriePage({super.key}) {
-    categoryBloc = ApiDataBloc<CategoryModel>()..add(const IndexDataEvent());
+    categoryBloc = ApiDataBloc<CategoryModel>()..add(
+      const IndexDataEvent(
+        // where: WhereCriteria(field: 'id',isEqualTo:  itemModel!.categoryId)
+    ));
   }
   
   @override
   PreferredSizeWidget? appBar(BuildContext context) => AppBar(
     leading: IconButton(
-      onPressed: () => Navigator.pushNamedAndRemoveUntil(context, appPageLayout, (route) => false),
+      onPressed: () => Navigator.pushReplacementNamed(context, adminHome),
       icon: const Icon(Icons.arrow_back_ios_new_rounded) 
     ),
     title: Text(
@@ -31,6 +35,8 @@ class CategoriePage extends AdminInterface{
 
   @override
   Widget buildBody(BuildContext context){
-    return CategoriesWidget(categoryBloc: categoryBloc);
+    return CategoriesWidget(
+      categoryBloc: categoryBloc,
+    );
   }
 }
