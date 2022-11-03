0c00ef242adf8e973085fb4a6bb6b12e9ea06096
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-11-03 16:45:07 +0200

    Update categories_widget.dart

--- lib/admin/categories/categories_widget/categories_widget.dart
+++ lib/admin/categories/categories_widget/categories_widget.dart
@@ -2,9 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:m_hany_store/admin/categories/categories_page/edit_item_categories_page.dart';
-import 'package:m_hany_store/admin/categories/categories_page/preview_item_categories_page.dart';
-import 'package:m_hany_store/admin/item/item_page/item_page.dart';
 import 'package:m_hany_store/core/bloc/bloc/api_data_bloc.dart';
 import 'package:m_hany_store/core/form_fields/button_form_feilds.dart';
 import 'package:m_hany_store/core/model/category_model.dart';
@@ -13,8 +10,8 @@ import 'package:m_hany_store/core/theme/colors/color_theme.dart';
 import 'package:m_hany_store/core/theme/fonts/style.dart';
 
 
+// ignore: must_be_immutable
 class CategoriesWidget extends StatefulWidget {
-
   final ApiDataBloc<CategoryModel> categoryBloc;
 
   const CategoriesWidget({
@@ -31,12 +28,11 @@ class _CategoriesWidgetState extends State<CategoriesWidget> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(12, 22, 12, 33),
+      padding: const EdgeInsets.fromLTRB(12, 22, 12, 22),
       child: Column(
         children: [
-          BlocConsumer(
+          BlocBuilder(
             bloc: widget.categoryBloc,
-            listener: (context, state) {},
             builder: (context, state) {
               if(state is DataLoadedState){
                 if(state.data == null || state.data.isEmpty){
@@ -49,8 +45,7 @@ class _CategoriesWidgetState extends State<CategoriesWidget> {
                     ),
                   );
                 }else{
-                  return SizedBox(
-                    height: 555,
+                  return Expanded(
                     child: SingleChildScrollView(
                       physics: const ScrollPhysics(),
                       child: ListView.separated(
@@ -78,7 +73,7 @@ class _CategoriesWidgetState extends State<CategoriesWidget> {
             height: 22,
           ),
           InkWell(
-            onTap: () => Navigator.pushNamed(context, addItemCategoriesPage),
+            onTap: () => Navigator.pushReplacementNamed(context, addItemCategoriesPage),
             child: FormFeilds.buttonFormField(
               title: 'Add  new Categories',
               dPadding: false,
@@ -96,109 +91,94 @@ class _CategoriesWidgetState extends State<CategoriesWidget> {
     required CategoryModel categoriesModel,
   }){
     return InkWell(
-      onTap: (){
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context){
-              return ShippingPage(categoriesModel: categoriesModel,);
-            }
+      onTap: ()=> Navigator.pushReplacementNamed(context, itemPage,arguments:  categoriesModel,),
+      borderRadius: BorderRadius.circular(8),//context, itemPage, arguments: categoriesModel
+      child: Card(
+        color: ColorTheme.porder,
+        child: Container(
+          width: double.infinity,
+          height: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            border: const Border(
+              bottom: BorderSide(color: ColorTheme.porder,width: 1),
+              left: BorderSide(color: ColorTheme.porder,width: 1),
+              right: BorderSide(color: ColorTheme.porder,width: 1),
+              top: BorderSide(color: ColorTheme.porder,width: 1),
+            ),
           ),
-        );
-      },
-      borderRadius: BorderRadius.circular(8),
-      child: Container(
-        width: double.infinity,
-        height: 100,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          border: const Border(
-            bottom: BorderSide(color: ColorTheme.porder,width: 1),
-            left: BorderSide(color: ColorTheme.porder,width: 1),
-            right: BorderSide(color: ColorTheme.porder,width: 1),
-            top: BorderSide(color: ColorTheme.porder,width: 1),
-          ),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const SizedBox(
-              width: 10,
-            ),
-            SizedBox(
-              width: 75,
-              height: 75,
-              child: PhysicalModel(
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                color: Colors.black,
-                shape: BoxShape.rectangle,  
-                borderRadius: BorderRadius.circular(8),
-                child: categoriesModel.image!.isNotEmpty? FadeInImage.assetNetwork(
-                  placeholder: 'assets/icons/lloading.gif',
-                  image: "${categoriesModel.image}",
-                  fit: BoxFit.fill,
-                  placeholderFit: BoxFit.contain,
-                ) : Image.asset('assets/images/no_image_available.jpg'),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                width: 10,
               ),
-            ),
-            const SizedBox(width: 18,),
-            Flexible(
-              fit: FlexFit.tight,
-              flex: 3,
-              child: Text(
-                categoriesModel.name,
-                style: getSemiBoldStyle(color: ColorTheme.wight,fontSize: 13,),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
+              SizedBox(
+                width: 75,
+                height: 75,
+                child: PhysicalModel(
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  color: Colors.black,
+                  shape: BoxShape.rectangle,  
+                  borderRadius: BorderRadius.circular(8),
+                  child: categoriesModel.image!.isNotEmpty? FadeInImage.assetNetwork(
+                    placeholder: 'assets/icons/lloading.gif',
+                    image: "${categoriesModel.image}",
+                    fit: BoxFit.fill,
+                    placeholderFit: BoxFit.contain,
+                  ) : Image.asset('assets/images/no_image_available.jpg'),
+                ),
               ),
-            ),
-            const Spacer(),
-            Row(
-              children: [ 
-                IconButton(
-                  onPressed: () => Navigator.push(
-                    context, MaterialPageRoute(
-                      builder: (context){
-                        return PreviewItemCategoriesPage(categoriesModel: categoriesModel,);
-                      }
-                    ),
-                  ),
-                  icon: FormFeilds.containerImage(assetImage: 'assets/images/eye.png',height: 18,width: 18),
+              const SizedBox(width: 18,),
+              Flexible(
+                fit: FlexFit.tight,
+                flex: 3,
+                child: Text(
+                  categoriesModel.name,
+                  style: getSemiBoldStyle(color: ColorTheme.wight,fontSize: 13,),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
                 ),
-                IconButton(
-                  onPressed: () => Navigator.push(
-                    context, MaterialPageRoute(
-                      builder: (context){
-                        return EditItemCategoriesPage(categoriesModel: categoriesModel,);
-                      }
-                    ),
-                  ),
-                  icon: FormFeilds.containerImage(assetImage: 'assets/images/edit.png',height: 18,width: 18),
-                ),
-                IconButton(
-                  onPressed: (){
-                    setState(() {  
-                      FormFeilds.deleteMessage(
-                        context, 
-                        'Are you sure to delete?',
-                        InkWell(
-                          onTap: () async {
-                            Navigator.of(context).pop();
-                            widget.categoryBloc.add(DeleteDataEvent(id: categoriesModel.id, files: const ['image']));
-                            // deleteItemCatecories(categoriesModel,).then((value) {
-                            //   Navigator.pushNamed(context, categoriesPage,);
-                            // });
-                          },
-                          child: FormFeilds.buttonFormField(title: 'delete',colorButton: ColorTheme.primary),
-                        ),
+              ),
+              const Spacer(),
+              Row(
+                children: [ 
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(
+                        context,
+                        previewItemCategoriesPage, 
+                        arguments: categoriesModel
                       );
-                    });
-                  }, 
-                  icon: FormFeilds.containerImage(assetImage: 'assets/images/delete.png',height: 18,width: 18),
-                ),
-              ],
-            ),
-          ],
+                    },
+                    icon: FormFeilds.containerImage(assetImage: 'assets/images/eye.png',height: 18,width: 18),
+                  ),
+                  IconButton(
+                    onPressed:  () => Navigator.pushReplacementNamed(context, editItemCategoriesPage ,arguments: categoriesModel),
+                    icon: FormFeilds.containerImage(assetImage: 'assets/images/edit.png',height: 18,width: 18),
+                  ),
+                  IconButton(
+                    onPressed: (){
+                      setState(() {  
+                        FormFeilds.deleteMessage(
+                          context, 
+                          'Are you sure to delete?',
+                          InkWell(
+                            onTap: () async {
+                              Navigator.of(context).pop();
+                              widget.categoryBloc.add(DeleteDataEvent(id: categoriesModel.id, files: const ['image']));
+                            },
+                            child: FormFeilds.buttonFormField(title: 'delete',colorButton: ColorTheme.primary),
+                          ),
+                        );
+                      });
+                    }, 
+                    icon: FormFeilds.containerImage(assetImage: 'assets/images/delete.png',height: 18,width: 18),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
