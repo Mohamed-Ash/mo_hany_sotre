fb51a6eac9ddc15609ff5e2babe77cb5c7f811b6
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-11-03 16:45:42 +0200

    Update color_theme.dart

--- lib/core/theme/colors/color_theme.dart
+++ lib/core/theme/colors/color_theme.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/cupertino.dart';
 
 class ColorTheme{
-  static const Color selectIconNavBar                          = Color(0xff6a77a0); //const Color(0xff282828), 
-  static const Color unSelectIconNavBar                        = Color(0xffCCCCCC); 
-  static const Color steam                                     = Color(0xff193446); 
-  static const Color textSteam                                 = Color(0xff55C0F4); 
-  static const Color rockStar                                  = Color(0xffFFAB00); 
-  // static const Color darkAppBar                             = Color(0xff282828); 
-  static const Color darkAppBar                                = Color(0xff262a2d); 
-  static const Color darkBackroundPage                         = Color(0xff111315);
   static const Color primary                                   = Color(0xff6610f2);
   static const Color wight                                     = Color(0xffFFFFFF);
-  static const Color backroundInput                            = Color(0xff0f1a2b);
-  static const Color titleTypePlay                             = Color(0xff303031);
   static const Color porder                                    = Color(0xff303031);
   static const Color hintText                                  = Color(0xff5e6676);
   static const Color authTitle                                 = Color(0xff585858);
+  static const Color darkAppBar                                = Color(0xff262a2d); 
+  static const Color selectIconNavBar                          = Color(0xff6a77a0); //const Color(0xff282828), 
+  static const Color unSelectIconNavBar                        = Color(0xffCCCCCC); 
+  static const Color darkBackroundPage                         = Color(0xff111315);
+  static const Color backroundInput                            = Color(0xff0f1a2b);
+  // static const Color titleTypePlay                             = Color(0xff303031);
 
+  // static const Color rockStar                                  = Color(0xffFFAB00); 
+  // static const Color steam                                     = Color(0xff193446); 
+  // static const Color textSteam                                 = Color(0xff55C0F4); 
+  // static const Color darkAppBar                             = Color(0xff282828); 
 }
