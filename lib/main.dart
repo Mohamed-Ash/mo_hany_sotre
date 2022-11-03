e895335863b1f941226eef87e85bc9ac406aa453
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-11-03 16:28:22 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:m_hany_store/core/app.dart';
 import 'package:m_hany_store/core/routes/route.dart';
@@ -5,14 +6,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:m_hany_store/firebase_options.dart';
 
 
-//6610f2
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  // ignore: avoid_print
+  print("Handling a background message: ${message.messageId}");
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp( App(routes: Routes(),));
-  
 }
