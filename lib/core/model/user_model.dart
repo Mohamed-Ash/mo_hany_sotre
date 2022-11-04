0b474c014c56386483ab5b9ac69b2efe734d4b54
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-11-04 12:00:03 +0200

    delete token

--- lib/core/model/user_model.dart
+++ lib/core/model/user_model.dart
@@ -1,22 +1,19 @@
 class UserModel {
   final String id;
   final String? email;
-  final String? token;
 
-  const UserModel({required this.id,this.email, this.token,});
+  const UserModel({required this.id,this.email,});
   
   factory UserModel.fromJson(Map<String,dynamic>json){
     return UserModel(
       id: json['id'],
       email: json['email'],
-      token: json['token'],
     );
   }
 
   Map<String,dynamic> tojson()=>{
     "id":id,
     "email":email,
-    "token":token,
   };
   
 }
