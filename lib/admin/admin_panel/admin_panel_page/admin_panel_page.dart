9505770e6dba7dfb81425acc789ec30ad663d5c1
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-11-05 21:16:47 +0200

    create admins & users page

--- lib/admin/admin_panel/admin_panel_page/admin_panel_page.dart
+++ lib/admin/admin_panel/admin_panel_page/admin_panel_page.dart
@@ -1,11 +1,27 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:m_hany_store/admin/admin_interface.dart';
+import 'package:m_hany_store/admin/admin_panel/admin_panel_widget/admin_panel_widget.dart';
+import 'package:m_hany_store/core/theme/colors/color_theme.dart';
+import 'package:m_hany_store/core/theme/fonts/style.dart';
 
 class AdminPanelPage extends AdminInterface{
   const AdminPanelPage({super.key});
 
   @override
+  PreferredSizeWidget? appBar(BuildContext context) => AppBar(
+    leading: IconButton(
+      onPressed: () => Navigator.pop(context),
+      icon: const Icon(Icons.arrow_back_ios_new_rounded) 
+    ),
+    centerTitle: true,
+    title: Text(
+      'Admin Panel',
+      style: getBoldStyle(color: ColorTheme.wight,fontSize: 22),
+    ),
+  );
+  
+  @override
   Widget buildBody(BuildContext context) {
-    return Column();
+    return const AdminPanelWidget();
   }
 }
--- lib/admin/admin_panel/admin_panel_widget/admin_panel_widget.dart
+++ lib/admin/admin_panel/admin_panel_widget/admin_panel_widget.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:m_hany_store/core/form_fields/button_form_feilds.dart';
+import 'package:m_hany_store/core/routes/string_route.dart';
+import 'package:m_hany_store/core/theme/colors/color_theme.dart';
+import 'package:m_hany_store/core/theme/fonts/style.dart';
 
-class AdminPanelWidget extends StatefulWidget {
+class AdminPanelWidget extends StatefulWidget {  
   const AdminPanelWidget({Key? key}) : super(key: key);
 
   @override
@@ -8,8 +12,72 @@ class AdminPanelWidget extends StatefulWidget {
 }
 
 class _AdminPanelWidgetState extends State<AdminPanelWidget> {
+  
   @override
   Widget build(BuildContext context) {
-    return Container();
+  return SingleChildScrollView(
+    child: Padding(
+      padding: const EdgeInsets.fromLTRB(15, 20, 15, 20),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () => Navigator.pushReplacementNamed(context, showAdminsPage),
+              child: Card(
+                color: ColorTheme.porder,
+                child: Container(
+                  width: double.infinity,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FormFeilds.containerImage(assetImage: 'assets/icons/manager.png'),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'Admins',
+                        style: getBoldStyle(color: ColorTheme.wight,fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+             const SizedBox(
+              height: 50,
+            ),
+            GestureDetector(
+              onTap: () => Navigator.pushReplacementNamed(context, showUsersPage),
+              child: Card(
+                color: ColorTheme.porder,
+                child: Container(
+                  width: double.infinity,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FormFeilds.containerImage(assetImage: 'assets/icons/un_select_profile.png',height: 40,width: 40),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'Users',
+                        style: getBoldStyle(color: ColorTheme.wight,fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
