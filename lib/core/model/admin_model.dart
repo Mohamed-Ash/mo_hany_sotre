87f23a67d4b9dcf4f71550808cdf3085afc3ca94
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-11-05 21:18:25 +0200

    Update admin_model.dart

--- lib/core/model/admin_model.dart
+++ lib/core/model/admin_model.dart
@@ -1,14 +1,20 @@
 class AdminModel{
   final String email;
+  final String id;
 
-  AdminModel({required this.email});
+  AdminModel({
+    required this.email,
+    required this.id
+  });
   
   factory AdminModel.fromJson(Map<String,dynamic> json){
     return AdminModel(
       email: json['email'],
+      id: json['id'],
     );
   }
   Map<String,dynamic> toJson() => {
-    "email":email
+    "email":email,
+    "id":id,
   };
 }
