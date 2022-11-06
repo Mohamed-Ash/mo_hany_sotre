e432e48498291db2f16fd640ecffc12bea146b6f
mohamedStore21234 <87867424+Mohamed-Ash@users.noreply.github.com>
2022-11-06 02:35:34 +0200

    Update profile_page.dart

--- lib/user/profile/profile_page/profile_page.dart
+++ lib/user/profile/profile_page/profile_page.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:m_hany_store/core/bloc/bloc/api_data_bloc.dart';
 import 'package:m_hany_store/core/model/admin_model.dart';
+import 'package:m_hany_store/core/model/user_model.dart';
 import 'package:m_hany_store/user/profile/profile_widget/profile_widget.dart';
 import 'package:m_hany_store/user/user_interface.dart';
 
 // ignore: must_be_immutable
 class ProfilePage extends UserInterface{
-  late ApiDataBloc<AdminModel> adminBloc;
+  late ApiDataBloc<AdminModel> adminModel;
+  late ApiDataBloc<UserModel> userModel;
 
   ProfilePage({super.key}){
-    adminBloc = ApiDataBloc<AdminModel>()..add(const IndexDataEvent());
+    adminModel = ApiDataBloc<AdminModel>()..add(const IndexDataEvent());
+    userModel = ApiDataBloc<UserModel>()..add(const IndexDataEvent());
   }
 
   @override
   Widget buildBody(BuildContext context) {
-    return ProfileWidget(adminBloc: adminBloc,);
+    return ProfileWidget(adminModel: adminModel,userModel: userModel,);
   }
 }
